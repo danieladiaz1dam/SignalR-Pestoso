86e8ba61e3b1fdd3b9b6a17ac6f52f5715883bbf
LaDaniWapa <urpopolakliente@gmail.com>
2025-01-27 14:39:17 +0100

    Try to add maui vm

---
+++ MAUI/Models/MainPageVM.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using MAUI.Models.Utils;
+using System.Collections.ObjectModel;
+using ENT;
+
+namespace MAUI.Models
+{
+    public class MainPageVM : VMBase
+    {
+        private HubConnection _connection;
+        private bool _canSend;
+        private bool _canConnect;
+        private DelegateCommand? _btnConectar, _btnDesconectar, _btnEnviar;
+        private ObservableCollection<Msg> _mensajes;
+
+        public bool CanSend { get => _canSend; }
+        public bool CanConnect { get => _canConnect; }
+        public DelegateCommand? BtnConectar => _btnConectar;
+        public DelegateCommand? BtnDesconectar => _btnDesconectar;
+        public DelegateCommand? BtnEnviar => _btnEnviar;
+        public ObservableCollection<Msg> Mensajes => _mensajes;
+
+        public MainPageVM()
+        {
+            _canSend = false;
+            _canConnect = true;
+            _connection = new HubConnectionBuilder()
+                .WithUrl($"http://localhost:5177/chatHub")
+                .Build();
+
+            initBtns();
+            _mensajes = new();
+
+            _connection.On<Msg>("ReceiveMessage", addMensaje);
+
+            Task.Run(() =>
+            {
+                Dispatcher.Dispatch(async () => await _connection.StartAsync());
+            });
+        }
+
+        private void addMensaje(Msg m)
+        {
+
+        }
+
+        async Task Connect()
+        {
+            await _connection.StartAsync();
+        }
+
+        async Task Disconnect()
+        {
+            await _connection.StopAsync();
+        }
+
+        #region Botones
+
+        private void initBtns()
+        {
+            _btnConectar = new DelegateCommand(_btnConectarCmd);
+            _btnDesconectar = new DelegateCommand(_btnDesconectarCmd);
+        }
+
+        private void _btnConectarCmd()
+        {
+            _canSend = true;
+            _canConnect = false;
+            NotifyPropertyChanged(nameof(CanSend));
+            NotifyPropertyChanged(nameof(CanConnect));
+        }
+
+        private void _btnDesconectarCmd()
+        {
+            _canSend = false;
+            _canConnect = true;
+            NotifyPropertyChanged(nameof(CanSend));
+            NotifyPropertyChanged(nameof(CanConnect));
+        }
+
+        #endregion
+    }
+}
---
+++ MAUI/Models/Utils/VMBase.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace MAUI.Models.Utils
+{
+    public abstract class VMBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void NotifyPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
